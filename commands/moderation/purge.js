7c2c64983564e592cc2a039ef91dbf58231270a4
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-07 19:12:39 -0500

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -4,7 +4,7 @@ module.exports = {
     config: {
         name: "purge",
         description: "Deletes an specific amount of messages.",
-        usage: "!purge <number>",
+        usage: "!purge <amount>",
         accessableby: "Helpers",
         aliases: ["clear"]
     },
@@ -29,9 +29,18 @@ module.exports = {
         .setColor(0xe20000)
         .setFooter(message.id)
         .setTimestamp();
-  
+        
+        let perembed = new RichEmbed()
+        .setTitle("Lucifer Bot")
+        .setDescription(`You do not have access to \`purge\`, ${message.author}.`)
+        .setColor(0xe20000)
+        .setFooter(message.id)
+        .setTimestamp();
+        
         if (isNaN(args[0])) return message.channel.send(amountembed)
         if(args[0] > 100) return message.channel.send(hundredembed)
+        
+        if(!message.author.hasPermissions("MANAGE_MESSAGES") return message.channel.send(permembed)
 
         message.channel.bulkDelete(args[0])
         .then(message.channel.send(successembed))
