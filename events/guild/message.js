b1edbf37271df46fc2a548c098e86ab6caf888c9
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-10 12:47:26 -0500

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -6,7 +6,17 @@ module.exports = async (bot, message) => {
     let args = message.content.slice(prefix.length).trim().split(/ +/g)
     let cmd = args.shift().toLowerCase()
     if(!message.content.startsWith(prefix)) return;
-
+    
+     let channelembed = new RichEmbed()
+     .setTitle("Lucifer Bot")
+     .setDescription(`Use the \`bot-commands\` channel to run commands, ${message.author}.`)
+     .setColor(0xe20000)
+     .setFooter(message.id)
+     .setTimestamp();
+    
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+    if(!commandfile.message.channels.find("574966288741302284")) {
+        if(!message.author.hasPermissions("MANAGE_MESSAGES")) return message.channel.send(channelembed);
+    }
     if(commandfile) commandfile.run(bot, message, args)
 }
